4aead8325e29575c0f85da91130bc5ddce1ff5a2
Javier Donoso <javier.donoso@nuilea.com>
2023-01-12 12:12:04 +0100

    synfee

--- src/CounterSyncFee.sol
+++ src/CounterSyncFee.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import {GelatoRelayContext} from "@gelatonetwork/relay-context/contracts/GelatoRelayContext.sol";
 
-contract Counter {
+contract CounterSyncFee {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
